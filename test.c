e1d5d66358f58f5d3dc254ea6c9145bb2b993bf9
Jelle Van kraaij <jvan-kra@f1r3s12.codam.nl>
2021-11-10 15:53:29 +0100

    working release

---
+++ test.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvan-kra <jvan-kra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 16:00:40 by jvan-kra          #+#    #+#             */
+/*   Updated: 2021/11/10 15:44:06 by jvan-kra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN    "\x1b[36m"
+#define COLOR_RESET  "\x1b[32m"
+
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "get_next_line.h"
+
+int g_free_count;
+int	g_malloc_count;
+int	g_malloc_target;
+
+void	*malloc(size_t size)
+{
+	void *res;
+	void	*(*real_malloc)(size_t);
+	real_malloc = dlsym(RTLD_NEXT, "malloc");
+	if (g_malloc_count + 1 >= g_malloc_target)
+	{
+		if (PRINT_ON)printf(COLOR_YELLOW "\nmalloc fail" COLOR_RESET);
+		return (NULL);
+	}
+	g_malloc_count++;
+	res = real_malloc(size);
+	if (PRINT_ON)printf(COLOR_YELLOW "\nmalloc %p %lu" COLOR_RESET, res, size);
+	return (res);
+}
+
+void	free(void *to_free)
+{
+	void	*(*real_free)(void *);
+
+	real_free = dlsym(RTLD_NEXT, "free");
+	real_free(to_free);
+	if (to_free != NULL)
+	{
+		g_free_count++;
+		if (PRINT_ON)printf(COLOR_YELLOW "\nfree %p" COLOR_RESET, to_free);
+	}
+}
+
+void	my_test(void)
+{
+	int		test = open(FILE_NAME, O_RDONLY);
+	char	*res = (char *)1;
+
+	while (res != NULL)
+	{
+		res = get_next_line(test);
+		if (PRINT_ON)printf("\n\"" COLOR_GREEN"%s"COLOR_RESET "\"", res);
+		free(res);
+	}
+	close(test);
+}
+
+int	main(void)
+{
+	while (g_malloc_target < 100)
+	{
+		g_malloc_count = 0;
+		g_free_count = 0;
+		if (PRINT_ON) printf("-----  running fail on malloc %d with buffersize %d -----", g_malloc_target, BUFFER_SIZE);
+		my_test();
+		if (PRINT_ON)printf("\n-----  result times malloc=%d free=%d -----\n\n", g_malloc_count, g_free_count);
+		if (g_malloc_count != g_free_count)
+		{
+			// printf(COLOR_RED "ERRORERRORERROR\n" COLOR_RESET);
+			exit(1);
+		}
+		g_malloc_target++;
+	}
+	return (0);
+}
---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DEBUG=0;
+
+for i in $(seq 1 5) $(seq 10 10 100)
+do
+	echo -n "Small File Running buffersize $i";
+	gcc -fsanitize=address -g3 -D PRINT_ON=$DEBUG -D BUFFER_SIZE=$i -D FILE_NAME=\"test1.txt\" test.c ../get_next_line.c ../get_next_line_utils.c && ./a.out && echo -e '\033[0;32m' " OK " '\033[0m' || { echo -e '\033[0;31m' " ERROR " '\033[0m'; exit; }
+done
+
+for i in $(seq 1 5) $(seq 10 10 100)
+do
+	echo -n "Big File Running buffersize $i";
+	gcc -fsanitize=address -g3 -D PRINT_ON=$DEBUG -D BUFFER_SIZE=$i -D FILE_NAME=\"test2.txt\" test.c ../get_next_line.c ../get_next_line_utils.c && ./a.out && echo -e '\033[0;32m' " OK " '\033[0m'|| { echo -e '\033[0;31m' " ERROR " '\033[0m'; exit; }
+done
+
+echo -n "Invalid file descriptor"
+gcc -fsanitize=address -g3 -D PRINT_ON=$DEBUG -D BUFFER_SIZE=$i -D FILE_NAME=\"NON_EXISTING\" test.c ../get_next_line.c ../get_next_line_utils.c && ./a.out && echo -e '\033[0;32m' " OK " '\033[0m' || echo -e '\033[0;31m' " ERROR " '\033[0m'
